2afe45e36265b28c6c9ee0e3f4cf96b3fb5728b5
Rocio Carmona <rociodelcarmen29022000@gmail.com>
2023-07-18 15:48:52 +0100

    lab 02

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,7 +4,7 @@ import "./Footer.css";
 export default function Footer() {
   return (
     <footer>
-      Author: Rocio Carmona
+      <p> Author: Rocio Carmona</p>
     </footer>
   )
 }
--- src/components/HornedBeast/HornedBeast.js
+++ src/components/HornedBeast/HornedBeast.js
@@ -1,12 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import "./HornedBeast.css";
 
 export default function HornedBeast({ title, imgUrl, description }) {
+  const [fav, setFav] = useState(0);
+
+  function handleFav(){
+    setFav(fav + 1);
+  }
+  
   return (
     <div className="hb-container">
       <h2>{title}</h2>
       <img src={imgUrl} alt={title} />
       <p>{description}</p>
+      <p>
+        <span className = "like" onClick ={handleFav}>
+        ❤️
+        </span> 
+        {fav}
+      </p>
     </div>
   );
 }
